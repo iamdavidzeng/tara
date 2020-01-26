8f26a97f9f5ed3451cb1e4a7cde09a1916478742
zzy <15779712170@163.com>
2020-01-26 19:39:03 +0800

    TARA-8 Build blog' CRUD with MongoDB

--- routes/router.go
+++ routes/router.go
@@ -1,8 +1,8 @@
 package routes
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"tara/services"
 )
 
@@ -14,13 +14,22 @@ func InitRoute() *gin.Engine {
 	})
 
 	userAPI := router.Group("/api/v1/users")
-	
+
 	{
 		userAPI.GET("/", services.GetUsers)
 		userAPI.GET("/:id", services.GetUser)
 		userAPI.POST("/", services.CreateUser)
 		userAPI.POST("/:id", services.UpdateUser)
 		userAPI.DELETE("/:id", services.DeleteUser)
+	}
+
+	postAPI := router.Group("/api/v1/posts")
+	{
+		postAPI.GET("/", services.GetPosts)
+		postAPI.GET("/:userId", services.GetPost)
+		postAPI.POST("/", services.CreatePost)
+		postAPI.POST("/:id", services.UpdatePost)
+		postAPI.DELETE("/:id", services.DeletePost)
 	}
 
 	return router

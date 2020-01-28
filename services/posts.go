e26f35fe7beb1af724b35db7eb17b8d4d9198830
zzy <15779712170@163.com>
2020-01-28 16:46:01 +0800

    Update and Delete

--- services/posts.go
+++ services/posts.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"tara/dependencies"
@@ -11,12 +12,29 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // GetPost Use to get one Post in mongodb
 func GetPost(ctx *gin.Context) {
-	return
+	var post models.Posts
+	docID := ctx.Param("id")
+	objectID, _ := primitive.ObjectIDFromHex(docID)
+
+	mongoCtx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	collection := dependencies.Client.Database("demo").Collection("posts")
+
+	if err := collection.FindOne(mongoCtx, bson.M{"_id": objectID}).Decode(&post); err != nil {
+		log.Fatal(err)
+	}
+
+	if post.Published != false {
+		postSchema := schemas.PostSchema{ID: post.ID, Title: post.Title, Content: post.Title, UpdatedAt: post.UpdatedAt, Published: post.Published}
+		ctx.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": postSchema})
+	} else {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "data": "Post Not Found."})
+	}
 }
 
 func GetPosts(ctx *gin.Context) {
@@ -28,7 +46,7 @@ func GetPosts(ctx *gin.Context) {
 	findOptions.SetLimit(10)
 
 	var postSchemas []schemas.PostSchema
-	cur, err := collection.Find(mongoCtx, bson.D{}, findOptions)
+	cur, err := collection.Find(mongoCtx, bson.M{"published": bson.M{"$eq": true}}, findOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,8 +56,9 @@ func GetPosts(ctx *gin.Context) {
 		if err = cur.Decode(&element); err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println(element)
 
-		postSchemas = append(postSchemas, schemas.PostSchema{Title: element.Title, Content: element.Content, UserID: element.UserID, UpdatedAt: element.UpdatedAt})
+		postSchemas = append(postSchemas, schemas.PostSchema{ID: element.ID, Title: element.Title, Content: element.Content, UserID: element.UserID, UpdatedAt: element.UpdatedAt, Published: element.Published})
 	}
 	if err := cur.Err(); err != nil {
 		log.Fatal(err)
@@ -66,9 +85,47 @@ func CreatePost(ctx *gin.Context) {
 }
 
 func UpdatePost(ctx *gin.Context) {
-	return
+	docID := ctx.Param("id")
+	objectID, _ := primitive.ObjectIDFromHex(docID)
+
+	var post schemas.PostSchema
+	ctx.BindJSON(&post)
+
+	mongoCtx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	collection := dependencies.Client.Database("demo").Collection("posts")
+
+	filter := bson.M{"_id": bson.M{"$eq": objectID}}
+	updateOptions := bson.M{"updated_at": time.Now()}
+	if post.Title != "" {
+		updateOptions["title"] = post.Title
+	}
+	if post.Content != "" {
+		updateOptions["content"] = post.Content
+	}
+	if post.Published == true {
+		updateOptions["published"] = true
+	}
+	update := bson.M{"$set": updateOptions}
+
+	result, err := collection.UpdateOne(mongoCtx, filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": result})
 }
 
 func DeletePost(ctx *gin.Context) {
-	return
+	docID := ctx.Param("id")
+	objectID, _ := primitive.ObjectIDFromHex(docID)
+
+	mongoCtx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	collection := dependencies.Client.Database("demo").Collection("posts")
+
+	result, err := collection.DeleteOne(mongoCtx, bson.M{"_id": objectID})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": result})
 }

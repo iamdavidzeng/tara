9f5c00dbc0bc4067a15232600baa34af55f47e12
iamdavidzeng <15779712170@163.com>
2021-05-14 19:02:30 +0800

    Escape import cycle

---
+++ api/schemas/schemas.go
@@ -0,0 +1,24 @@
+package schemas
+
+import (
+	"time"
+)
+
+type (
+	UserSchema struct {
+		ID       uint   `json:"id"`
+		Email    string `json:"email"`
+		Phone    string `json:"phone"`
+		Password string `json:"password"`
+	}
+
+	PostSchema struct {
+		ID        int       `json:"id"`
+		UserID    int       `json:"user_id"`
+		Title     string    `json:"title"`
+		Content   string    `json:"content"`
+		Published bool      `json:"published"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+)
---
+++ api/services/users/client.go
@@ -0,0 +1,82 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/iamdavidzeng/tara/api/models"
+	"github.com/iamdavidzeng/tara/api/schemas"
+	"github.com/iamdavidzeng/tara/internal/db"
+)
+
+func New(c *gin.Context) {
+	var user schemas.UserSchema
+	c.BindJSON(&user)
+
+	_user := models.Users{Email: user.Email, Phone: user.Phone, Password: user.Password}
+	db.Conn.DB.Create(&_user)
+
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": "Create successfully!"})
+}
+
+func Get(c *gin.Context) {
+	var user models.Users
+	userID := c.Param("id")
+
+	db.Conn.DB.First(&user, userID)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "data": "User Not Found!"})
+		return
+	}
+
+	_user := schemas.UserSchema{ID: user.ID, Email: user.Email, Phone: user.Phone, Password: user.Password}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _user})
+}
+
+func List(c *gin.Context) {
+	var users []models.Users
+	var _users []schemas.UserSchema
+
+	db.Conn.DB.Find(&users)
+	if len(users) <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusUnauthorized, "data": "User Not Found!"})
+		return
+	}
+
+	for _, user := range users {
+		_users = append(_users, schemas.UserSchema{ID: user.ID, Email: user.Email, Phone: user.Phone, Password: user.Password})
+	}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _users})
+}
+
+func Update(c *gin.Context) {
+	var user models.Users
+	userID := c.Param("id")
+
+	db.Conn.DB.First(&user, userID)
+	if user.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "data": "User Not Found!"})
+		return
+	}
+
+	var userData schemas.UserSchema
+	c.BindJSON(&userData)
+
+	db.Conn.DB.Model(&user).Update("email", userData.Email)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": "Update successfully!"})
+}
+
+func Delete(c *gin.Context) {
+	var user models.Users
+	userID := c.Param("id")
+
+	db.Conn.DB.First(&user, userID)
+	if user.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "data": "User Not Found!"})
+		return
+	}
+
+	db.Conn.DB.Delete(&user)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": "Delete successfully!"})
+}
